f101dfc3c6a1844e9cb821be585796b2d4b2368e
fujiisui <sui_face@yahoo.co.jp>
2024-11-17 16:40:15 +0900

    Completed (tentative)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :content, length: {in: 1..140, 
+                              too_long: "投稿は最大140文字です",
+                              too_short: "投稿は最小1文字です",
+                              }
 end
