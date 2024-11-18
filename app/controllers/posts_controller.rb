b2941891e5f926e8dd2f50070edf60c2e0700a2f
fujiisui <sui_face@yahoo.co.jp>
2024-11-18 22:43:21 +0900

    Validation update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,14 +23,11 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.valid?
+      @post.save
+        redirect_to @post, notice: "投稿が作成されました"
+    else
+      render :new
     end
   end
 
