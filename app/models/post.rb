7d8313a93befa9642bae3989af14a1ecf46ea290
fujiisui <sui_face@yahoo.co.jp>
2024-11-18 19:13:22 +0900

    refactoring alidates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
-  validates :content, length: {in: 1..140, 
-                              too_long: "投稿は最大140文字です",
-                              too_short: "投稿は最小1文字です",
-                              }
+  validates :content, length: {
+    in: 1..140,
+    too_long: "投稿は最大140文字です",
+    too_short: "投稿は最小1文字です"
+  }
 end
